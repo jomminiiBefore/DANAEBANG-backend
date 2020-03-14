c160356f857ff42f585967682cfe80d7be87db3b
이종민 <>
2020-03-14 15:05:19 +0900

    단지 실제 거래히스토리 뷰 작성

--- danaebang/settings.py
+++ danaebang/settings.py
@@ -146,19 +146,19 @@ CORS_ALLOW_HEADERS = (
 )
 
 ##debug
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-    'console': {
-        'level': 'DEBUG',
-        'class' : 'logging.StreamHandler',
-    }
-  },
-  'loggers': {
-    'django.db.backends':{
-      'handlers': ['console'],
-      'level': 'DEBUG',
-    },
-  }
-}
+#LOGGING = {
+#    'version': 1,
+#    'disable_existing_loggers': False,
+#    'handlers': {
+#    'console': {
+#        'level': 'DEBUG',
+#        'class' : 'logging.StreamHandler',
+#    }
+#  },
+#  'loggers': {
+#    'django.db.backends':{
+#      'handlers': ['console'],
+#      'level': 'DEBUG',
+#    },
+#  }
+#}
--- room/views.py
+++ room/views.py
@@ -1,8 +1,11 @@
-from .models                import Complex 
+import datetime
+
+from .models                import Complex, ComplexSpaceInfo, ComplexPriceInfo
 
 from django.views           import View
 from django.http            import JsonResponse, HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models       import Avg
 
 class DetailView(View):
     def get(self, request):
@@ -27,7 +30,7 @@ class DetailView(View):
                     'build_cov_ratio'                   : complex.build_cov_ratio,
                     'floor_area_index'                  : complex.floor_area_index,
                     'image_urls'                        : [image.image_url for image in complex.compleximage_set.all()],
-                    'trade_average_pyeong_price'        : complex.complex_price_info.trade_average_pyeong_price,
+                    'trade_average_pyeong_price'        : (complex.complex_price_info.trade_average_pyeong_price),
                     'lease_average_pyeong_price'        : complex.complex_price_info.lease_average_pyeong_price,
                     'trade_region_average_pyeong_price' : complex.complex_price_info.trade_region_average_pyeong_price,
                     'lease_region_average_pyeong_price' : complex.complex_price_info.lease_region_average_pyeong_price,
@@ -50,3 +53,79 @@ class DetailView(View):
                 pass
         except Complex.DoesNotExist:
             return JsonResponse({"message": "INVALID_COMPLEX_ID"}, status = 400)
+
+class TradeHistoryView(View):
+    def get(self, request):
+        try:
+            id                = request.GET.get('id', None)
+            now               = datetime.datetime.now().strftime('%Y%m')
+            three_year_before = f'{int(now[:4])-3}{now[4:]}'
+            pyeong            = (
+                ComplexSpaceInfo
+                .objects
+                .prefetch_related('tradehistory_set')
+                .get(id = id)
+            )
+
+            rent_month            = [date_dict['date'] for date_dict in pyeong.tradehistory_set
+                                     .filter(date__gte = three_year_before, trade_type_id = 1)
+                                     .values('date').distinct()]
+            monthly_rent_lists    = [pyeong
+                                     .tradehistory_set
+                                     .filter(date = date, trade_type_id = 1)
+                                     .all() for date in rent_month]
+
+            lease_month           = [date_dict['date'] for date_dict in pyeong.tradehistory_set
+                                     .filter(date__gte = three_year_before, trade_type_id = 2)
+                                     .values('date').distinct()]
+            monthly_lease_lists   = [pyeong
+                                     .tradehistory_set
+                                     .filter(date = date, trade_type_id = 2)
+                                     .all() for date in lease_month]
+
+            selling_month         = [date_dict['date'] for date_dict in pyeong.tradehistory_set
+                                     .filter(date__gte = three_year_before, trade_type_id = 3)
+                                     .values('date').distinct()]
+            monthly_selling_lists = [pyeong
+                                     .tradehistory_set
+                                     .filter(date = date, trade_type_id = 3)
+                                     .all() for date in selling_month]
+
+            results = {
+                'rent_history' : [{
+                    'date'            : monthly_rent[0].date,
+                    'rent_count'      : monthly_rent.count(),
+                    'histories'       : [{
+                        'type'    : rent.trade_type.name,
+                        'date'    : rent.date,
+                        'deposit' : rent.deposit,
+                        'price'   : rent.price,
+                        'floor'   : rent.floor
+                        } for rent in monthly_rent]
+                    } for monthly_rent in monthly_rent_lists],
+                'lease_history' : [{
+                    'date'            : monthly_lease[0].date,
+                    'lease_count'     : monthly_lease.count() ,
+                    'average_deposit' : int(monthly_lease.aggregate(Avg('deposit'))['deposit__avg']),
+                    'histories'       : [{
+                        'type'    : lease.trade_type.name,
+                        'date'    : lease.date,
+                        'deposit' : lease.deposit,
+                        'floor'   : lease.floor
+                        } for lease in monthly_lease]
+                    } for monthly_lease in monthly_lease_lists],
+                'selling_history' : [{
+                    'date'            : monthly_selling[0].date,
+                    'selling_count'   : monthly_selling.count(),
+                    'average_deposit' : int(monthly_selling.aggregate(Avg('deposit'))['deposit__avg']),
+                    'histories'       : [{
+                        'type'    : selling.trade_type.name,
+                        'date'    : selling.date,
+                        'deposit' : selling.deposit,
+                        'floor'   : selling.floor
+                        } for selling in monthly_selling]
+                    } for monthly_selling in monthly_selling_lists]
+                }
+            return JsonResponse({"result":results}, status = 200)
+        except ComplexSpaceInfo.DoesNotExist:
+            return JsonResponse({"message":"INVALID_COMPLEXSPACEINFO_ID"}, status = 400)
